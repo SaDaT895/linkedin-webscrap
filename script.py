5b35628531d986d376711d2e7a8f8c543b282dae
Sadat Ahmad <s.ahmad-1@student.utwente.nl>
2022-07-10 17:34:57 +0530

    Basic structure with parser

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import selenium
 from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import InvalidArgumentException
 import argparse
 
 parser = argparse.ArgumentParser(description="Scrap information for given LinkedIn URL")
@@ -14,3 +15,12 @@ opts = webdriver.ChromeOptions()
 opts.headless = True
 driver = webdriver.Chrome(ChromeDriverManager().install())
 url = args.url[0]
+
+try:
+    driver.get(url)
+    assert "https://www.linkedin.com/" in driver.current_url
+    
+except InvalidArgumentException:
+    print("Not a valid URL")
+except AssertionError:
+    print("Not a LinkedIn URL")
